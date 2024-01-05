3eee68c8ed9712673f4a9795aa4f6cad4e4a33ea
Muraliyohan <muralid156@gmail.com>
2024-01-05 23:21:56 +0530

    3rd script

---
+++ 04-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+
+echo "$PERSON1: Hello $PERSON2, Good morning"
+echo "$PERSON2: Hi $PERSON1, very Good morning"
+echo "$PERSON3: Hey $PERSON1 and $PERSON2 am good"
+echo "$PERSON1: How are you $PERSON2 and $PERSON3"
+echo "$PERSON2: I am good, $PERSON1 How re you"
+echo "$PERSON1: I am also good, $PERSON2"
+echo "$PERSON2: I am also, $PERSON1"
