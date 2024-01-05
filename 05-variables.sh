1aa3164b36b07c111c4a94a917025c476cf40641
Muraliyohan <muralid156@gmail.com>
2024-01-05 23:32:55 +0530

    3rd script

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,5 @@
 
 echo " Pleae enter your username"
 
-read USERNAME # the value entered above will be automatically attached to USERNAME variable
+read -s USERNAME # the value entered above will be automatically attached to USERNAME variable
 
-echo "Username is: $USERNAME"
