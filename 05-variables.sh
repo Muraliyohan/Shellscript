885496846499e85c7ce9558c58af81f1ccd75db0
Muraliyohan <muralid156@gmail.com>
2024-01-05 23:27:47 +0530

    3rd script

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+echo " Pleae enter your username"
+
+read USERNAME # the value entered above will be automatically attached to USERNAME variable
 USERNAME=$1
 PASSWORD=$2
 
-echo "Username is: $USERNAME, Password is: $PASSWORD"
+echo "Username is: $USERNAME"
