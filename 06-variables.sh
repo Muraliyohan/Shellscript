9ae4711e6e9c429ab2a32aa1906590dca0560226
Muraliyohan <muralid156@gmail.com>
2024-02-06 08:56:52 +0530

    ravi added

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-echo "Cnu: ekadra"
-echo "Murali: intlo"
-echo "Cnu: nee yava chennai ah"
-echo "Murali: raamaapuram"
-echo "Cnu: ne vasta with magnet "
-echo "Murali: vadura saamy"
-echo "Cnu: ravi also coming with me"
-echo "Murali: andhuke vadana"
-echo "Cnu: vaadu thagithe manishi kaadu"
-echo "Murali: i know man"
+PERSON1=Cnu
+PERSON2=Murali
+PERSON3=Ravi
+
+echo "$PERSON1: ekadra"
+echo "$PERSON2: intlo"
+echo "$PERSON1: nee yava chennai ah"
+echo "$PERSON2: raamaapuram"
+echo "$PERSON1: ne vasta with magnet "
+echo "$PERSON2: vadura saamy"
+echo "$PERSON1: ravi also coming with me"
+echo "$PERSON2: andhuke vadana"
+echo "$PERSON1: vaadu thagithe manishi kaadu"
+echo "$PERSON2: i know man"
+echo "$PERSON3: Iron vanchala emti"
+echo "$PERSON1: paduko nenu vuna kada"
+echo "$PERSON2: Khansaar erupekala"
