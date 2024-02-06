e83d2fa3ec6b2faffd9b799f29a92f2a456faf6c
Muraliyohan <muralid156@gmail.com>
2024-02-06 08:29:41 +0530

    updated script

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,8 +4,9 @@ echo " Pleae enter your username"
 
 read -s USERNAME # the value entered above will be automatically attached to USERNAME variable
 
+echo "Username is: $USERNAME" # I am printing just for validation, you should not print username and passwords in scripts
 
 
 echo "Please enter your Password:: "
 read -s PASSWORD
-
+echo "Username is: $USERNAME, Password is: $PASSWORD " # I am printing just for validates , you should not print username and passwords in scripts
---
+++ 06-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Cnu: ekadra"
+echo "Murali: intlo"
+echo "Cnu: nee yava chennai ah"
+echo "Murali: raamaapuram"
+echo "Cnu: ne vasta with magnet "
+echo "Murali: vadura saamy"
