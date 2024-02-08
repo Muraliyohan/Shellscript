c6b281530db2f57a6f8d02418c3ea3ef87a24289
Muraliyohan <muralid156@gmail.com>
2024-02-08 23:06:52 +0530

    conditions

---
+++ 07-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FRUITS=("Apple" "Banana" "Orange" "Mango")
+
+echo "First value: ${FRUITS[0]}"
+echo "Second value: ${FRUITS[1]}"
+echo "Third value: ${FRUITS[2]}"
+echo "Fourth value: ${FRUITS[3]}"
+echo "All fruits: ${FRUITS[@]}"
