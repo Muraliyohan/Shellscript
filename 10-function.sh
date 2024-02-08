83c26f56977ae1fbe4cd52796c97f0bb21ea1312
Muraliyohan <muralid156@gmail.com>
2024-02-09 00:57:14 +0530

    sql error

---
+++ 10-function.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+        echo "ERROR: Installing MySQL is failed"
+        exit 1
+    else
+        echo "Installing MYSQL is SUCESS"
+    fi
+}
+
+    if [ $ID -ne 0 ]
+    then    
+        echo "ERROR:: Please run this script with root access"
+        exit 1 # you can give other than 0
+    else
+        echo "you are root user"
+    fi #fi means reverse of if, indicating condition end
+
+yum install mysql -y
